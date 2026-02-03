cc67ee3c9fce3166c88c939fd948c98953712ccc
nmajestic <njmajestic98@gmail.com>
2026-02-03 15:47:44 -0800

    Hopefully this fixed the function signature errors

--- src/main.c
+++ src/main.c
@@ -90,9 +90,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	if (addstring) {
-		dbhdr->count++;
-		employees = realloc(employees, dbhdr->count*(sizeof(struct employee_t)));
-		add_employee(dbhdr, employees, addstring);
+		add_employee(dbhdr, &employees, addstring);
 	}
 
 	if (list) {
